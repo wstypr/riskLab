c9c35219a28a50340695ca780d3eadd04429e7db
wstypr <wsetypeermadi@gmail.com>
2024-11-08 01:28:47 +0700

    fix bussiness process prompt

--- src/lib/fetchBussinessProcess.ts
+++ src/lib/fetchBussinessProcess.ts
@@ -12,13 +12,13 @@ export default async function fetchBussinessProcess({
   kegiatan: string;
 }) {
   const response = await openai.chat.completions.create({
-    model: "gpt-4o",
+    model: "gpt-4o-mini",
     messages: [
       {
         role: "system",
         content: [
           {
-            text: `anda adalah ahli dalam perencanaan pemerintah. anda akan diberikan nama sebuah organisasi perangkat daerah dan sebuah program beserta indikator keberhasilannya serta kegiatan-kegiatan yang ada dalam program tersebut. identifikasi proses bisnis program tersebut dengan menimbang indikator keberhasilan dan kegiatan-kegiatan yang ada di dalamnya.\nperangkat daerah: ${perangkatDaerah}\nprogram: ${program}\nkegiatan: ${kegiatan}\nindikator program: ${indikator} \n\nCRITICAL:\nRespond in Bahasa Indonesia.`,
+            text: `anda adalah ahli dalam perencanaan pemerintah. anda akan diberikan nama sebuah organisasi perangkat daerah dan sebuah program beserta indikator keberhasilannya serta kegiatan-kegiatan yang ada dalam program tersebut. identifikasi proses bisnis program tersebut dengan menimbang indikator keberhasilan dan kegiatan-kegiatan yang ada di dalamnya.\nperangkat daerah: ${perangkatDaerah}\nprogram: ${program}\nkegiatan: ${kegiatan}\nindikator program: ${indikator} \n\nCRITICAL: EXPLAIN THE BUSSINESS PROCESS IN DETAIL\n\nCRITICAL:Respond in Bahasa Indonesia.`,
             type: "text",
           },
         ],
